ec5a0050de19cfd1f26f54673fd6e64cfe918562
tobiascabrera-cano <tobias.cabrera@gmx.de>
2015-05-22 13:46:00 +0200

    update

--- src/components/kundenverwaltung/listKunden/listKundenController.js
+++ src/components/kundenverwaltung/listKunden/listKundenController.js
@@ -34,32 +34,53 @@
         'agbAkzeptiert': true,
         'bestellungenUri': 'https://localhost:8443/shop/rest/bestellungen/kunde/300'
     },{
-        'type': 'F',
+        'type': 'P',
         'version': 0,
-        'id': 300,
+        'id': 301,
         'identity': {
-            'loginname': 'admin',
+            'loginname': 'Peter',
             'enabled': true,
-            'nachname': 'Admin',
-            'email': 'admin@hs-karlsruhe.de',
+            'nachname': 'Peter',
+            'email': 'peter@hs-karlsruhe.de',
             'adresse': {
-                'plz': '76133',
+                'plz': '76137',
                 'ort': 'Karlsruhe',
                 'strasse': 'Moltkeweg',
-                'hausnr': '1'
+                'hausnr': '3'
             }
         },
         'kategorie': 1,
-        'seit': '2014-02-01',
+        'seit': '2014-04-01',
         'newsletter': false,
         'agbAkzeptiert': true,
-        'bestellungenUri': 'https://localhost:8443/shop/rest/bestellungen/kunde/300'
+        'bestellungenUri': 'https://localhost:8443/shop/rest/bestellungen/kunde/301'
+    },{
+        'type': 'P',
+        'version': 0,
+        'id': 302,
+        'identity': {
+            'loginname': 'Hans',
+            'enabled': true,
+            'nachname': 'Hans',
+            'email': 'hans@hs-karlsruhe.de',
+            'adresse': {
+                'plz': '76137',
+                'ort': 'Karlsruhe',
+                'strasse': 'Moltkeweg',
+                'hausnr': '3'
+            }
+        },
+        'kategorie': 1,
+        'seit': '2014-01-01',
+        'newsletter': false,
+        'agbAkzeptiert': true,
+        'bestellungenUri': 'https://localhost:8443/shop/rest/bestellungen/kunde/301'
     }];
         }
     }
 
-    // "Property Annotation" fuer DI: fuer den Modus 'strict' und fuer Minification
-    listKundenController.$inject = [];
+    // // "Property Annotation" fuer DI: fuer den Modus 'strict' und fuer Minification
+    // listKundenController.$inject = [];
 
     /* global angular: false */
     angular.module('appKundenverwaltung').controller('listKundenController', listKundenController);
